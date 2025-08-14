978ac337944b7fed136470223078506535064ce3
Rajvir <rajvir.singh.120@my.csun.edu>
2025-08-13 21:46:33 -0700

    Database connection for node.js

---
+++ DBConnectNode.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const path = require('path');
+const mysql2 = require('mysql2');
+const app = express();
+
+
+const database = mysql2.createConnection({
+  host: "hoteldatabase.cx0k04cysbk2.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "Comp380DataBase",
+  database: "HotelDB"
+});
+
+database.connect((error) => {
+    if(error)
+    {
+        return console.error(error)
+    }
+
+    console.log("Mysql database is connected....")
+})
+
+app.get('/', (req, res) =>{
+    const htmlfile = path.join(__dirname, 'index.html');
+    res.sendFile(htmlfile);
+})
+
